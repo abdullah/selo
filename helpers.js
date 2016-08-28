accc0e3d5690b3f6f49d7e1f8a0416da1f50b90d
Abdullah Marax <abdullah.mara@bodevoffice.com>
2016-08-28 23:15:33 +0300

    Hello

---
+++ helpers.js
@@ -0,0 +1,11 @@
+
+module.exports = function getParents(el) {
+  var a = el;
+  var parents = [];
+  
+  while (a) {
+      parents.unshift(a);
+      a = a.parentNode;
+  }
+  return parents;
+}
---
+++ selo.js
@@ -0,0 +1,167 @@
+/*
+* 28 August 2016 Abdullah Mara  @abdullah
+*/
+
+var getParents = require('./helpers')
+
+;(function () {
+	
+	function Selo(props) {
+		if (!props.el) {
+        
+        var log = props.log != undefined ? props.log : true
+
+        if (log) {
+          console.log("[El parameters] is not defined, this element set as body  ")
+        }
+		}
+
+		if (!document) {
+			throw "[Platfrom] not supported"
+		}
+
+		this.arena      = null;
+		this.selection  = null;
+		this.el         = props.el || document.body;
+
+		this.init()
+	}
+
+	Selo.prototype.init = function () {
+		this.arena = this.el;
+		this.selection = window.getSelection()
+
+		this.attachEvent();
+	}
+
+	Selo.prototype.attachEvent = function () {
+
+       var selectionEndTimeout = null,
+          endEvent            = new CustomEvent('selectionEnd'),
+          startEvent          = new CustomEvent('selectionStart',{detail:this.selection}),
+          beforeStartEvent    = new CustomEvent('selectionBeforeStart');
+            
+
+      var map =  {65: false, 91: false};
+      
+      var listener = [
+        "mouseup",
+        "selectionchange",
+        "keyup",
+        "keydown"
+      ].map(function(e) {
+        document.addEventListener(e.toString(), function(evt/*event*/) {
+          
+
+          if (evt.type != "selectionchange" && evt.type != "mouseup") {
+
+            if (evt.type == "keydown") {
+              map[evt.keyCode] = true
+
+            }else{
+              
+              if (map[65] && map[91] ) {
+                if (this.hasText()) {
+                  document.dispatchEvent(endEvent);
+                }
+              }
+
+              Object.keys(map).map(function(e){
+                map[e] = false
+              })
+
+            }
+
+          }
+
+
+          if (evt.type == "selectionchange") {
+            if (this.selection.type == "Range") {
+              if (this.hasText()) {
+                  document.dispatchEvent(startEvent);
+              }
+            }else{
+                if (this.inArena()) {
+                  document.dispatchEvent(beforeStartEvent);
+                }
+            }
+            clearTimeout(selectionEndTimeout);
+          }
+
+          selectionEndTimeout = setTimeout(function(){
+              if (evt.type == "mouseup" && this.hasText()) {
+                  document.dispatchEvent(endEvent);
+              }
+          }.bind(this), 100);
+        }.bind(this))
+      }.bind(this))
+
+    }
+	
+	
+	Selo.prototype.hasText = function () {
+      if (this.inArena()) {
+        return  this.selection.toString() != ""
+      }
+      return false
+    }
+
+    Selo.prototype.inArena = function () {
+      if (this.selection.focusNode) {
+        var parents = getParents(this.selection.focusNode.parentNode);
+        return parents.indexOf(this.arena) != -1;
+      }
+      return false
+    }
+
+    Selo.prototype.getPositionRange = function () {
+      
+      var oRange = this.selection.getRangeAt(0); //get the text range
+      var oRect = oRange.getBoundingClientRect();
+
+      var bound = {};
+
+      bound["left"] =  oRect.left
+      bound["top"] =  oRect.top
+      bound["getBoundingClientRect"] = oRect;
+      return  bound
+    }
+    
+    Selo.prototype.saveSelection = function ()  {
+        if (window.getSelection) {
+            var sel = window.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                return sel.getRangeAt(0);
+            }
+        } else if (document.selection && document.selection.createRange) {
+            return document.selection.createRange();
+        }
+        return null;
+    }
+
+    Selo.prototype.restoreSelection  = function (range) {
+        if (range) {
+            if (window.getSelection) {
+                var sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(range);
+            } else if (document.selection && range.select) {
+                range.select();
+            }
+        }
+    }
+
+
+	if (typeof exports == "object") {
+		module.exports = Selo
+	} else if (typeof define == "function" && define.amd) {
+	define([], function () {
+		return Selo
+	})
+	} else if (window.Vue) {
+		window.Selo = Selo
+	}
+
+
+
+})()
